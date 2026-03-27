950f442172cb606da497409c3f9bc3087932a740
Aditya9869z <adityaa0001v@gmail.com>
2026-03-27 18:20:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const text = "Aditya - Backend Developer";
+let i = 0;
+
+function typeEffect(){
+  if(i < text.length){
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100);
+  }
+}
+typeEffect();
